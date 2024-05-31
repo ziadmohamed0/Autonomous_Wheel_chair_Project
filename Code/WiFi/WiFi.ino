25d9a527347cf0728c5f6a500b2f9b3565c242e4
ziadmohamed0 <zizo.alprnc.90@gmail.com>
2024-05-31 17:23:16 +0300

    Modifycode

--- Code/WiFi/WiFi.ino
+++ Code/WiFi/WiFi.ino
@@ -1,58 +1,26 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-
-// Replace with your Wi-Fi credentials
+#include "INIT_Init.h"
+/****** Wi-Fi credentials ******/
 const char* ssid = "realme C67";
-const char* password = "zohirr5000";
+const char* password = "123456789";
 
-#define IN_1_L D1
-#define IN_2_L D2
-#define IN_3_R D3
-#define IN_4_R D4
-
-#define BUZZER	D0
-
-#define TRIG D5
-#define ECHO D6
+/****** Rooms Flag ******/
+unsigned int Flage_Rooms = 0;
 
 // Initialize the web server on port 80
 ESP8266WebServer server(80);
 
-// Define the handler for the root path ("/")
-void handleRoot() {
-
-  String html = "<!DOCTYPE html><html><head>";
-    html += "<title>Zzrad</title><style>.button-container { display: flex; justify-content: center; align-items: center; height: 100vh; }";
-    html += ".button1 { background-color: #202467; color: #ffffff; font-size: xx-large; }";
-    html += ".button2 { background-color: #202467; color: #ffffff; font-size: xx-large; }";
-    html += ".button3 { background-color: #202467; color: #ffffff; font-size: xx-large; }";
-    html += "</style></head><body>";
-    html += "<div class=\"button-container\"><div><h1>Autonomous Chair system</h1>";
-    html += "<button class=\"button1\" onclick=\"sendNumber(1)\">Laboratory</button><br>";
-    html += "<button class=\"button2\" onclick=\"sendNumber(2)\">X-ray lab</button><br>";
-    html += "<button class=\"button3\" onclick=\"sendNumber(3)\">Pharmacy</button><br>";
-    html += "</div></div>";
-    html += "<script>function sendNumber(num) { var xhr = new XMLHttpRequest(); xhr.open('GET', '/send?number=' + num, true); xhr.send(); }";
-    html += "</script></body></html>";
-    
-    server.send(200, "text/html", html);
-
-  // Create a simple HTML page with three buttons
-}
-
-// Define the handler for the "/send" path
-void handleSend() {
+/****** Initialization Program ******/
+void handleSend()
+{
   // Get the "number" parameter from the URL query string
   String number = server.arg("number");
-  
-  // Print the number to the serial monitor (you can add your custom actions here)
+
+  // Print the number to the serial monitor.
   Serial.print("Received number: ");
   Serial.println(number);
-  
-  // Respond to the client
-  server.send(200, "text/plain", "Number received");
-  int num = number.toInt();
-   int Distance = 0;
+  int Distance = 0;
   int Duration = 0;
 
   digitalWrite(TRIG, LOW);
@@ -64,220 +32,106 @@ void handleSend() {
   Duration = pulseIn(ECHO, HIGH);
 
   Distance = Duration / 57.14;
-  if (num == 1)
-  {
-    if (Distance < 10)
-    {
-      digitalWrite(IN_1_L, LOW);
-      digitalWrite(IN_2_L, LOW);
+  // Respond to the client
+  server.send(200, "text/plain", "Number received");
+  int num = number.toInt();
 
-      digitalWrite(IN_3_R, LOW);
-      digitalWrite(IN_4_R, LOW);
-      delay(25);
-      digitalWrite(BUZZER, HIGH);
-      
+  if (Distance < 10) 
+  {
+    digitalWrite(BUZZ, HIGH);
+    delay(2000);
+    digitalWrite(BUZZ, LOW);
+    Motor_Stop();
+    delay(25);
+  } 
+  else 
+  {
+    digitalWrite(BUZZ, LOW);
+    if (num == 0)
+    {
+      Home();
+      server.send(200, "text/plain", "ENTERED Home");
+      Serial.println("ENTERED Home");
+      Serial.println(Distance);
+      delay(500);
     }
-    else
+
+    if (num == 1)
     {
-    digitalWrite(BUZZER, LOW);
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-    
-    delay(2000);
-    
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);  
-    digitalWrite(IN_3_R, LOW);
-    digitalWrite(IN_4_R, LOW);
-    
-    delay(3000);
-
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-
-    delay(4000);
-   
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);  
-    digitalWrite(IN_3_R, LOW);
-    digitalWrite(IN_4_R, LOW);
-   
-    delay(2000);
-    
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-    
-    delay(2000);
-  
-    digitalWrite(IN_1_L, LOW);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, LOW);
-    digitalWrite(IN_4_R, LOW);
-   
-    delay(10000);
-    server.send(200, "text/plain", "ENTERED ROOM 1");
-    Serial.println("ENTERED Laboratory");
-    Serial.println(Distance);
-    delay(500);
-    }  // go to first room
-  }
-  else if (num == 2)
-  {
+      pharmacy();
+      server.send(200, "text/plain", "ENTERED ROOM 1");
+      Serial.println("ENTERED pharmacy");
+      Serial.println(Distance);
+      delay(500);
+    }
+    if (num == 2) 
+    {
       // Tgo to second room
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-    
-    delay(2000);
-    
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);  
-    digitalWrite(IN_3_R, LOW);
-    digitalWrite(IN_4_R, LOW);
-    
-    delay(3000);
-
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-
-    delay(4000);
-   
-    digitalWrite(IN_1_L, LOW);
-    digitalWrite(IN_2_L, LOW);  
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-   
-    delay(2000);
-    
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-    
-    delay(2000);
-  
-    digitalWrite(IN_1_L, LOW);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, LOW);
-    digitalWrite(IN_4_R, LOW);
-   
-    delay(10000);
-
-    server.send(200, "text/plain", "ENTERRD ROOM 2");
-    Serial.println("ENTERED X-Ray Lab");
-  }
-    else if (num == 3)
-  {
+      X_ray();
+      server.send(200, "text/plain", "ENTERRD ROOM 2");
+      Serial.println("ENTERED X-Ray Lab");
+    }
+    if (num == 3) 
+    {
       // Tgo to second room
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-    
-    delay(1000);
-    
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);  
-    digitalWrite(IN_3_R, LOW);
-    digitalWrite(IN_4_R, LOW);
-    
-    delay(4000);
-
-    digitalWrite(IN_1_L, HIGH);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, HIGH);
-    digitalWrite(IN_4_R, LOW);
-
-    delay(4000);
-   
-    digitalWrite(IN_1_L, LOW);
-    digitalWrite(IN_2_L, LOW);
-    digitalWrite(IN_3_R, LOW);
-    digitalWrite(IN_4_R, LOW);
-   
-    delay(10000);
-
-    server.send(200, "text/plain", "ENTERRD ROOM 2");
-    Serial.println("ENTERED Pharmacy");
+      laboratory();
+      server.send(200, "text/plain", "ENTERRD ROOM 3");
+      Serial.println("ENTERED laboratory");
+    }
   }
 }
 
-void setup() {
+void setup() 
+{
   // Start the serial monitor
   Serial.begin(115200);
 
   // Connect to the Wi-Fi network
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-  
+
   Serial.println("Connected to WiFi");
   Serial.println("IP address: " + WiFi.localIP().toString());
-  
+
   // Set up the handlers for the web server
   server.on("/", handleRoot);
   server.on("/send", handleSend);
 
   // Start the web server
   server.begin();
-
-
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
 
-  pinMode(IN_1_L, OUTPUT);
-  pinMode(IN_2_L, OUTPUT);
-
-  pinMode(IN_3_R, OUTPUT);
-  pinMode(IN_4_R, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
-
+  pinMode(IN_1, OUTPUT);
+  pinMode(IN_2, OUTPUT);
+  pinMode(IN_3, OUTPUT);
+  pinMode(IN_4, OUTPUT);
+  pinMode(BUZZ, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   // Handle incoming client requests
+  int Distance = 0;
+  int Duration = 0;
+
+  digitalWrite(TRIG, LOW);
+  delay(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  Duration = pulseIn(ECHO, HIGH);
+
+  Distance = Duration / 57.14;
+
   server.handleClient();
- 
-  //    int Distance = 0;
-  // int Duration = 0;
-
-  // digitalWrite(TRIG, LOW);
-  // delay(2);
-  // digitalWrite(TRIG, HIGH);
-  // delayMicroseconds(10);
-  // digitalWrite(TRIG, LOW);
-
-  // Duration = pulseIn(ECHO, HIGH);
-
-  // Distance = Duration / 57.14;
-
-  // if (Distance < 10)
-  //  {
-  //     digitalWrite(IN_1_L, LOW);
-  //     digitalWrite(IN_2_L, LOW);
-
-  //     digitalWrite(IN_3_R, LOW);
-  //     digitalWrite(IN_4_R, LOW);
-  //     delay(25);
-  //     digitalWrite(BUZZER, HIGH);
-      
-  //   }
-  //   else
-  //   {
-  //      digitalWrite(BUZZER, LOW);
-  //   }
-  
-  // Serial.println(Distance);
+  Serial.println(Distance);
   delay(500);
 }
+
+
